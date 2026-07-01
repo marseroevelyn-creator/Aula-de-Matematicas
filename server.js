25dc368cfd1d80f5180af48c517302cdf27f4617
Eve marsero <marseroevelyn@gmail.com>
2026-07-01 10:15:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -486,4 +486,6 @@ app.post('/api/sistema/restaurar', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`[OK] Servidor corriendo en http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running and listening on port ${PORT}`);
+});
